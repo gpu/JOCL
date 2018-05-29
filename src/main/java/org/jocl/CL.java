09746373ca3191a095a3e322dec39004f451d21f
Marco Hutter <jocl@jocl.org>
2018-05-29 17:47:14 +0200

    Updated version numbers to 2.0.1

--- src/main/java/org/jocl/CL.java
+++ src/main/java/org/jocl/CL.java
@@ -58,7 +58,7 @@ public final class CL
     {
         if (!nativeLibraryLoaded)
         {
-            String versionString = "2_0_0";
+            String versionString = "2_0_1";
             String libraryBaseName = "JOCL_" + versionString;
             String libraryName = 
                 LibUtils.createPlatformLibraryName(libraryBaseName);
